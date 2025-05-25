05c62e2c32ec769f6f6665863f5fb2c6e6fc6829
LiaWritesCode <liaramos.dev@outlook.com>
2025-05-25 00:12:36 -0400

    Initialize CartItem.jsx

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,36 +1,55 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { removeItem, updateQuantity } from './CartSlice';
+import { addItem, removeItem, updateQuantity } from './CartSlice';
 import './CartItem.css';
+import './ProductList';
 
 const CartItem = ({ onContinueShopping }) => {
-  const cart = useSelector(state => state.cart.items);
+  const cart = useSelector(state => state.cart);
+  console.log("Cart component loading. Cart state:", useSelector(state => state.cart));
+  const cartItem = cart?.items || [];
   const dispatch = useDispatch();
 
   // Calculate total amount for all products in the cart
-  const calculateTotalAmount = () => {
- 
+const calculateTotalAmount = () => {
+    return cart.reduce((total, item) => total + parseFloat(item.cost.substring(1)) * item.quantity, 0).toFixed(2);
   };
 
-  const handleContinueShopping = (e) => {
-   
+const handleContinueShopping = (e) => {
+    e.preventDefault();
   };
 
-
-
-  const handleIncrement = (item) => {
+const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+    };
+    
+const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
-  const handleDecrement = (item) => {
-   
+const handleDecrement = (item) => {
+    if (item.quantity > 1) {
+      dispatch(updateQuantity(item.name, item.quantity - 1));
+    } 
+    else {
+      dispatch(removeItem(item.name));
+    }
   };
 
-  const handleRemove = (item) => {
+const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    let unitPrice = parseFloat(item.cost.substring(1));
+    return (unitPrice * item.quantity).toFixed(2);
   };
+
+    const calculateTotalQuantity = (cart) => {
+    return cart ? cart.reduce((total, item) => total + item.quantity, 0) : 0;
+    };
+
 
   return (
     <div className="cart-container">
@@ -53,7 +72,9 @@ const CartItem = ({ onContinueShopping }) => {
           </div>
         ))}
       </div>
-      <div style={{ marginTop: '20px', color: 'black' }} className='total_cart_amount'></div>
+      <div style={{ marginTop: '20px', color: 'black' }} className='total_cart_amount'>
+        {calculateTotalQuantity(cart)}
+</div>
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
@@ -64,5 +85,3 @@ const CartItem = ({ onContinueShopping }) => {
 };
 
 export default CartItem;
-
-
