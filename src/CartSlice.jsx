507645ef0b33e580f5180badda14211e56734e57
LiaWritesCode <liaramos.dev@outlook.com>
2025-05-25 01:02:03 -0400

    go back to initial state for cartslice

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,38 +1,19 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { useDispatch, useSelector } from 'react-redux';
-import './CartItem.css';
-import './ProductList';
 
 export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
     items: [], // Initialize items as an empty array
-    addedItems: {}
   },
   reducers: {
     addItem: (state, action) => {
-    const { name, image, cost } = action.payload;
-    const existingItem = state.items.find(item => item.name === name);
-  if (existingItem) {
-    existingItem.quantity++;
-  } else {
-    state.items.push({ name, image, cost, quantity: 1 });
-    state.addedItems[name] = true; 
-  }
-},
+    
+    },
+    removeItem: (state, action) => {
+    },
+    updateQuantity: (state, action) => {
 
     
-    removeItem: (state, action) => {
-        state.items = state.items.filter(item => item.name !== action.payload);
-       
-    },
-    
-    updateQuantity: (state, action) => {
-    const { name, quantity } = action.payload;
-    console.log(`Before update:`, state.items);
-    const itemToUpdate = state.items.find(item => item.name === name);
-if (itemToUpdate) {itemToUpdate.quantity = quantity;}
-    console.log(`Redux state after update:`, state.items);
     },
   },
 });
