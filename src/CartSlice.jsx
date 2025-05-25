9569aaf5f0523e91d4a21acde34fb497a0d1f4e0
LiaWritesCode <liaramos.dev@outlook.com>
2025-05-25 00:15:06 -0400

    Add CartSlice.jsx

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,19 +1,38 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { useDispatch, useSelector } from 'react-redux';
+import './CartItem.css';
+import './ProductList';
 
 export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
     items: [], // Initialize items as an empty array
+    addedItems: {}
   },
   reducers: {
     addItem: (state, action) => {
-    
-    },
-    removeItem: (state, action) => {
-    },
-    updateQuantity: (state, action) => {
+    const { name, image, cost } = action.payload;
+    const existingItem = state.items.find(item => item.name === name);
+  if (existingItem) {
+    existingItem.quantity++;
+  } else {
+    state.items.push({ name, image, cost, quantity: 1 });
+    state.addedItems[name] = true; 
+  }
+},
 
     
+    removeItem: (state, action) => {
+        state.items = state.items.filter(item => item.name !== action.payload);
+       
+    },
+    
+    updateQuantity: (state, action) => {
+    const { name, quantity } = action.payload;
+    console.log(`Before update:`, state.items);
+    const itemToUpdate = state.items.find(item => item.name === name);
+if (itemToUpdate) {itemToUpdate.quantity = quantity;}
+    console.log(`Redux state after update:`, state.items);
     },
   },
 });
